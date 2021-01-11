590eeb0d266258e3205acfdd97f9b34eb3ab0e08
Sarah <uhno22@163.com>
2021-01-11 18:26:54 +0800

    demo

---
+++ src/views/ActivityEntry.vue
@@ -0,0 +1,11 @@
+<script lang='jsx'>
+import { defineComponent, getCurrentInstance } from 'vue'
+const ActivityEntry = defineComponent({
+  setup () {
+    const { ctx } = getCurrentInstance()
+    ctx.$toast('测试')
+    return () => {}
+  }
+})
+export default ActivityEntry
+</script>
